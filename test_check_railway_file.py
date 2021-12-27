bf931755b7c65c0156263be8a3565bae21b3999a
Pelmen323 <nozovka9@mail.ru>
2021-12-27 17:07:50 +0300

    Some text lines on setup and teardowm

--- test_check_railway_file.py
+++ test_check_railway_file.py
@@ -6,11 +6,12 @@ def open_railways_file(filepath: str) -> str:
         with open(filepath, 'r') as raw_text_file:
             return raw_text_file.read()
     except Exception as ex:
-        print(ex)
+        print("Unable to open railways file", ex)
 
 @pytest.mark.parametrize("path_to_railway_file", 
     [("C:\\Users\\VADIM\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\Kaiserreich Dev Build\\map\\railways.txt")])
 def test_check_railways_file(path_to_railway_file: str) -> bool:
+    print("The railway file test is started! Opening the file...")
     lines = open_railways_file(path_to_railway_file).split('\n')
     line_counter = 0
     for line in lines:
@@ -24,6 +25,7 @@ def test_check_railways_file(path_to_railway_file: str) -> bool:
         str_with_provinces = line[4:].strip()
         list_with_provinces = str_with_provinces.split()
         assert int(counter_of_provinces_in_line) == len(list_with_provinces), f"Line {line_counter} - expected {counter_of_provinces_in_line} provinces, got {list_with_provinces}. Line: {line}"
+    print("The test is finished!")
 
 
 if __name__ == '__main__':
