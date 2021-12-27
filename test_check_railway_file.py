4035777029798a024aa7ed07179494544e7b51ab
Pelmen323 <nozovka9@mail.ru>
2021-12-27 17:26:04 +0300

    Test will not stop on AssertionError

--- test_check_railway_file.py
+++ test_check_railway_file.py
@@ -14,6 +14,7 @@ def test_check_railways_file(path_to_railway_file: str) -> bool:
     print("The railway file test is started! Opening the file...")
     lines = open_railways_file(path_to_railway_file).split('\n')
     line_counter = 0
+    errors_list = []
     for line in lines:
         line_counter += 1
         if line == '':
@@ -24,7 +25,10 @@ def test_check_railways_file(path_to_railway_file: str) -> bool:
             counter_of_provinces_in_line = line[2]
         str_with_provinces = line[4:].strip()
         list_with_provinces = str_with_provinces.split()
-        assert int(counter_of_provinces_in_line) == len(list_with_provinces), f"Line {line_counter} - expected {counter_of_provinces_in_line} provinces, got {list_with_provinces}. Line: {line}"
+        if int(counter_of_provinces_in_line) != len(list_with_provinces):
+            errors_list.append(f"Line {line_counter} - expected {counter_of_provinces_in_line} provinces, got {list_with_provinces}. Line: {line}")
+    if errors_list != []:
+        raise AssertionError(errors_list)
     print("The test is finished!")
 
 
