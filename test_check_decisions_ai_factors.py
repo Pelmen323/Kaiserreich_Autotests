921c42d371a0e38b2a7e7690b574bfa929b83233
Pelmen323 <nozovka9@mail.ru>
2022-01-09 02:05:32 +0300

    Added more info to the hint, added files to skip

--- test_check_decisions_ai_factors.py
+++ test_check_decisions_ai_factors.py
@@ -3,7 +3,14 @@ import glob
 import pytest
 import re
 FILEPATH = "C:\\Users\\VADIM\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\Kaiserreich Dev Build\\common\\decisions\\"
-FILES_TO_SKIP = ('00_demobilization_decisions.txt', 'ZZ_debug_decisions.txt')
+FILES_TO_SKIP = ('00_demobilization_decisions.txt', 
+                'ZZ_debug_decisions.txt', 
+                'Cyrenaica_decisions.txt',      # Caravans
+                'Germany_decisions.txt',        # Empty decisions with modifier
+                'Hunan_decisions.txt',          # Until reworked
+                'Intermarium_decisions.txt',    # 2 non-ai decisions
+                'New_England_decisions.txt',    # 9 empty decisions
+                )
 
 @pytest.mark.parametrize("filepath", [(FILEPATH)])
 def test_check_decisions_ai_factors(filepath: str):
@@ -33,7 +40,7 @@ def test_check_decisions_ai_factors(filepath: str):
         if expected_num_of_ai_factors > ai_will_do_counter:
             results_dict[file] = f'There are more decisions and selectable missions in the file ({expected_num_of_ai_factors}) than ai factors ({ai_will_do_counter}). Not all decisions will be available for AI!)'
         elif expected_num_of_ai_factors < ai_will_do_counter:
-            results_dict[file] = f'Huh? We found {ai_will_do_counter} ai factors and only {expected_num_of_ai_factors} selectable decisions and missions. Lets recheck that file... Probably not all decisions have icons'
+            results_dict[file] = f'Huh? We found {ai_will_do_counter} ai factors and only {expected_num_of_ai_factors} selectable decisions and missions. Lets recheck that file... Probably not all decisions have icons or missions that are not selectable have ai factors'
 
     if results_dict != {}:
         for error in results_dict.items():
