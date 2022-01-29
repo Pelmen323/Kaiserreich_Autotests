5423e3d88c7e559290e6444951e5f22daad141c4
Pelmen323 <nozovka9@mail.ru>
2022-01-29 16:15:49 +0300

    Add new autotest - check if cleared global event targets are set at least once

---
+++ tests/test_check_cleared_event_targets.py
@@ -0,0 +1,53 @@
+##########################
+# Test script to check for event targets that are cleared but not set
+# By Pelmen, https://github.com/Pelmen323
+##########################
+import glob
+import re
+from .imports.file_functions import open_text_file
+
+
+def test_check_cleared_event_targets(test_runner: object):
+    filepath = test_runner.full_path_to_mod
+    event_targets = {}
+# Part 1 - get the dict of all global flags
+    for filename in glob.iglob(filepath + '**/*.txt', recursive=True):
+        try:
+            text_file = open_text_file(filename)
+        except Exception as ex:
+            print(f'Skipping the file {filename}')
+            print(ex)
+            continue
+
+        if 'event_target:' in text_file:
+            event_targets_in_file = re.findall('clear_global_event_target = \\w*\\b', text_file)
+            if len(event_targets_in_file) > 0:
+                for target in event_targets_in_file:
+                    target = target[27:].strip()
+                    event_targets[target] = 0
+
+
+# Part 2 - count the number of flag occurrences
+    print(f'{len(event_targets)} cleared event targets found')
+    for filename in glob.iglob(filepath + '**/*.txt', recursive=True):
+        try:
+            text_file = open_text_file(filename)
+        except Exception as ex:
+            print(f'Skipping the file {filename}')
+            print(ex)
+            continue
+
+        not_encountered_targets = [i for i in event_targets.keys() if event_targets[i] == 0]
+
+        if 'save_global_event_target_as =' in text_file:
+            for target in not_encountered_targets:
+                event_targets[target] += text_file.count(f'save_global_event_target_as = {target}')
+
+
+# Part 3 - throw the error if flag is not used
+    results = [i for i in event_targets if event_targets[i] == 0]
+    if results != []:
+        print("Following event targets are cleared but never set:")
+        for i in results:
+            print(f'- [ ] {i}')
+        raise AssertionError("Missing event targets were encountered! Check console output")
