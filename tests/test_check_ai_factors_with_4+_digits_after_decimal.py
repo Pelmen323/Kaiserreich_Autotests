808cb9fb4195cd55a07bd67213a2b9cead2489a0
Pelmen323 <nozovka9@mail.ru>
2022-02-08 23:01:41 +0300

    Add new test - check for AI factors with 4+ digits after decimal point

---
+++ tests/test_check_ai_factors_with_4+_digits_after_decimal.py
@@ -0,0 +1,38 @@
+##########################
+# Test script to check if ai factors with 4+ digits after decimal point are used
+# Hoi4 supports only 3 digits after decimal, ignoring it results in huge ussues with the factors (like '0.4286' results in '4' factor instead)
+# See here for proof https://hoi4.paradoxwikis.com/AI_modding
+# By Pelmen, https://github.com/Pelmen323
+##########################
+import glob
+import os
+import re
+from .imports.file_functions import open_text_file
+import logging
+
+
+def test_check_negative_ai_factors(test_runner: object):
+    filepath = test_runner.full_path_to_mod
+    results = {}
+    for filename in glob.iglob(filepath + '**/*.txt', recursive=True):
+        try:
+            text_file = open_text_file(filename)
+        except Exception as ex:
+            logging.warning(f'Skipping the file {filename}')
+            logging.warning(ex)
+            continue
+
+        text_file_splitted = text_file.split('\n')
+        for line in range(len(text_file_splitted)):
+            current_line = text_file_splitted[line-1]
+            if 'factor =' in current_line:
+                pattern_matches = re.findall('\tfactor = \d\.\d{4,10}', current_line)
+                if len(pattern_matches) > 0:
+                    results[f'{os.path.basename(filename)}, line {line}'] = current_line
+
+    if results != {}:
+        logging.warning("Factors with 4+ digits after decimal point found!:")
+        for i in results.items():
+            logging.error(f'- [ ] {i}')
+        logging.warning(f'{len(results)} factors with 4+ digits after decimal point found.')
+        raise AssertionError("Factors with 4+ digits after decimal point found, Hoi4 factors supports only 3 digits after decimal point! Check console output")
