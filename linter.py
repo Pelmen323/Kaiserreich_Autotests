3c087c0b04e5a605a37d680f20a954d0f9df7421
Pelmen323 <nozovka9@mail.ru>
2022-04-09 16:12:20 +0400

    Update linter - empty lines purging

--- linter.py
+++ linter.py
@@ -2,12 +2,15 @@ import glob
 import re
 from test_classes.generic_test_class import FileOpener, DataCleaner
 from core.runner import TestRunner
-FILES_TO_SKIP = ['localisation', 'interface', 'gfx', 'map', 'common\\units', 'names']
+FILES_TO_SKIP = ['localisation', 'interface', 'gfx', 'map', 'common\\units', 'names', 'states']
 
 
-def replace_string(filename, pattern, replace_with, encoding="utf-8"):
+def replace_string(filename, pattern, replace_with, encoding="utf-8", flag=None):
     text_file = FileOpener.open_text_file(filename, lowercase=False)
-    text_file_fixed = re.sub(pattern=pattern, repl=replace_with, string=text_file)
+    if flag is None:
+        text_file_fixed = re.sub(pattern=pattern, repl=replace_with, string=text_file)
+    else:
+        text_file_fixed = re.sub(pattern=pattern, repl=replace_with, string=text_file, flags=flag)
 
     with open(filename, 'w', encoding=encoding) as text_file_write:
         text_file_write.write(text_file_fixed)
@@ -21,12 +24,14 @@ def apply_linting(filename, encoding="utf-8"):
     replace_string(filename=filename, pattern='\\{(?=[\\w_\\"=])', replace_with='{ ', encoding=encoding)        # Add spaces between symbol and {
     replace_string(filename=filename, pattern='(?<=[\\w_\\"=])\\}', replace_with=' }', encoding=encoding)       # Add spaces between symbol and }
     replace_string(filename=filename, pattern='(?<=[^\\n])\\Z', replace_with='\\n', encoding=encoding)          # Add last line if file is missing
-    replace_string(filename=filename, pattern='[ \t]+$', replace_with="", encoding=encoding)                    # Remove last line spaces
+    replace_string(filename=filename, pattern='^$\\n{2,}', replace_with='\\n', encoding=encoding, flag=re.MULTILINE)          # Add last line if file is missing
+    # replace_string(filename=filename, pattern='[ \t]+$', replace_with="", encoding=encoding)                    # Remove last line spaces
 
 
 def lint_kaiserreich(username, mod_name):
     runner = TestRunner(username, mod_name)
     filepath_common = f'{runner.full_path_to_mod}common\\'
+    filepath_history = f'{runner.full_path_to_mod}history\\'
     filepath_events = f'{runner.full_path_to_mod}events\\'
     filepath_unit_names_divisions = f'{runner.full_path_to_mod}common\\units\\names_divisions\\'
     filepath_unit_names_ships = f'{runner.full_path_to_mod}common\\units\\names_ships\\'
@@ -35,6 +40,11 @@ def lint_kaiserreich(username, mod_name):
         if DataCleaner.skip_files(files_to_skip=FILES_TO_SKIP, filename=filename):
             continue
         apply_linting(filename=filename)
+
+    for filename in glob.iglob(filepath_history + '**/*.txt', recursive=True):
+        if DataCleaner.skip_files(files_to_skip=FILES_TO_SKIP, filename=filename):
+            continue
+        apply_linting(filename=filename, encoding="utf-8-sig")
 
     for filename in glob.iglob(filepath_events + '**/*.txt', recursive=True):
         apply_linting(filename=filename, encoding="utf-8-sig")
