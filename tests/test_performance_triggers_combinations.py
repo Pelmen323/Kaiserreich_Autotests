cd17ad4b12f8f711713bc0af016ebefb39394187
Pelmen323 <nozovka9@mail.ru>
2022-06-13 16:29:03 +0400

    Update formatter to purge version control, add "unlock_decision" test:
    -  to detect "unlock_decision" tooltips for potential investingation

--- tests/test_performance_triggers_combinations.py
+++ tests/test_performance_triggers_combinations.py
@@ -210,3 +210,21 @@ def test_check_set_state_category(test_runner: object):
 
 # Part 2 - throw the error if those combinations are encountered
     ResultsReporter.report_results(results=results, message="set_state_category is encountered - use 'increase_state_category_by_one_level = yes' instead. Check console output")
+
+
+def test_non_targeted_decisions(test_runner: object):
+    filepath = test_runner.full_path_to_mod
+    results = []
+
+# Part 1 - perform search
+    for filename in glob.iglob(filepath + '**/*.txt', recursive=True):
+        text_file = FileOpener.open_text_file(filename)
+
+        if 'unlock_decision' in text_file:
+            pattern_matches = re.findall('.*unlock_decision.*', text_file)
+            if len(pattern_matches) > 0:
+                for match in pattern_matches:
+                    results.append((match.replace('\t', ''), os.path.basename(filename)))
+
+# Part 2 - throw the error if those combinations are encountered
+    ResultsReporter.report_results(results=results, message="Recheck the piece of code and convert to targeted decision if possible. Check console output")
