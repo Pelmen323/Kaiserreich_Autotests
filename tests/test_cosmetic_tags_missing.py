207cb2b04236875465d6666e604b851b0683fb80
Pelmen323 <nozovka9@mail.ru>
2022-08-15 21:21:52 +0400

    Maintenance update

--- tests/test_cosmetic_tags_missing.py
+++ tests/test_cosmetic_tags_missing.py
@@ -7,7 +7,9 @@ import logging
 import os
 import re
 
-from ..test_classes.generic_test_class import FileOpener, ResultsReporter
+from ..test_classes.generic_test_class import FileOpener, ResultsReporter, DataCleaner
+
+FALSE_POSITIVES = ["cro_habsburg"]
 
 
 def test_check_cosmetic_tags_missing(test_runner: object):
@@ -19,10 +21,9 @@ def test_check_cosmetic_tags_missing(test_runner: object):
         text_file = FileOpener.open_text_file(filename)
 
         if 'has_cosmetic_tag =' in text_file:
-            pattern_matches = re.findall('has_cosmetic_tag = \\b\\w*\\b', text_file)
+            pattern_matches = re.findall('has_cosmetic_tag = ([^ \n\t]+)', text_file)
             if len(pattern_matches) > 0:
                 for match in pattern_matches:
-                    match = match[19:].strip().strip('}').strip()
                     cosmetic_tags[match] = 0
                     paths[match] = os.path.basename(filename)
 
@@ -38,5 +39,6 @@ def test_check_cosmetic_tags_missing(test_runner: object):
                 cosmetic_tags[flag] += text_file.count(f'set_cosmetic_tag = {flag}')
 
 # Part 3 - throw the error if tag is not used
+    cosmetic_tags = DataCleaner.clear_false_positives(input_iter=cosmetic_tags, false_positives=FALSE_POSITIVES)
     results = [i for i in cosmetic_tags if cosmetic_tags[i] == 0]
     ResultsReporter.report_results(results=results, paths=paths, message="Missing cosmetic tags were encountered. Check console output")
