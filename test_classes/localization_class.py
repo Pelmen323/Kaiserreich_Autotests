c77a6029e591b3b2a7437ad5b0ddc7e6df33dfe5
Pelmen323 <nozovka9@mail.ru>
2022-09-07 16:20:47 +0400

    Add new test - duplicated loc keys

--- test_classes/localization_class.py
+++ test_classes/localization_class.py
@@ -6,10 +6,11 @@ from .generic_test_class import FileOpener
 class Localization:
 
     @classmethod
-    def get_all_loc_keys(cls, test_runner, lowercase: bool = True) -> dict:
+    def get_all_loc_keys(cls, test_runner, lowercase: bool = True, return_duplicated_keys: bool = False) -> dict:
         filepath = f'{test_runner.full_path_to_mod}localisation\\'
         results = []
         loc_dict = {}
+        duplicated_loc_keys = []
 
         for filename in glob.iglob(filepath + '**/*.yml', recursive=True):
             if lowercase:
@@ -17,13 +18,22 @@ class Localization:
             else:
                 text_file = FileOpener.open_text_file(filename, lowercase=False)
 
-            lines_raw = text_file.split('\n')                                           # 1. Get list of all lines regardless of contents
-            lines_raw = [i for i in lines_raw if ":" in i and "l_english:" not in i]    # 2. Form a list only with valid loc keys
+            if "l_english" not in text_file:
+                continue
+
+            lines_raw = text_file.split('\n')                                                                           # 1. Get list of all lines regardless of contents
+            lines_raw = [i for i in lines_raw if ":" in i and "l_english:" not in i and i[0] != "#" and i[1] != "#"]    # 2. Form a list only with valid loc keys
             results += lines_raw
 
         for i in results:
-            key = i[:i.index(":")].strip()                                                      # 3. Process the list with all loc and make it a dict
+            key = i[:i.index(":")].strip()                                                                              # 3. Process the list with all loc and make it a dict
             value = i[i.index(":") + 2:].strip()
-            loc_dict[key] = value
+            if key in loc_dict.keys():
+                duplicated_loc_keys.append(key)
+            else:
+                loc_dict[key] = value
 
-        return loc_dict
+        if return_duplicated_keys:
+            return loc_dict, duplicated_loc_keys
+        else:
+            return loc_dict
---
+++ tests/test_localisation_keys_duplicated.py
@@ -0,0 +1,12 @@
+##########################
+# Find duplicated loc keys
+# By Pelmen, https://github.com/Pelmen323
+##########################
+from ..test_classes.generic_test_class import ResultsReporter
+from ..test_classes.localization_class import Localization
+
+
+def test_find_duplicated_keys(test_runner: object):
+    loc_keys, duplicated_keys = Localization.get_all_loc_keys(test_runner=test_runner, return_duplicated_keys=True)
+
+    ResultsReporter.report_results(results=duplicated_keys, message="Duplicated loc keys were encountered. Check console output")
