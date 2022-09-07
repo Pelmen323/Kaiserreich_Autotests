314ced4526a6c4b11ba9b2006352716c26fa5305
Pelmen323 <nozovka9@mail.ru>
2022-09-07 18:33:56 +0400

    Expand formatter functionality - add whitespace to the key line if it doesn't have one

--- formatter.py
+++ formatter.py
@@ -63,6 +63,7 @@ def apply_formatting_loc(filename, encoding="utf-8-sig"):
     replace_string(filename=filename, pattern='(?<=[^\\n])\\Z', replace_with='\\n', encoding=encoding)          # Add last line if file is missing
     replace_string(filename=filename, pattern=':[0-9] ', replace_with=': ', encoding=encoding)                      # Purge version control
     replace_string(filename=filename, pattern='^$\\n{2,}', replace_with='\\n', encoding=encoding, flag=re.MULTILINE)          # Add last line if file is missing
+    replace_string(filename=filename, pattern='^([^ #\\n]+?:) ', replace_with=' \\1 ', encoding=encoding, flag=re.MULTILINE)  # Add whitespace if the line doesn't start with
     # replace_string(filename=filename, pattern='[ \t]+$', replace_with="", encoding=encoding)                    # Remove last line spaces
 
 
