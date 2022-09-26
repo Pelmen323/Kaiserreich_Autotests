014a3d83f28e4a4c4cd79c8fa6ac1bedf0b377e2
Pelmen323 <nozovka9@mail.ru>
2022-09-26 13:00:48 +0400

    Fix duplicated keys test reporting keys that have different case

--- tests/test_localisation_keys_duplicated.py
+++ tests/test_localisation_keys_duplicated.py
@@ -7,6 +7,6 @@ from ..test_classes.localization_class import Localization
 
 
 def test_find_duplicated_keys(test_runner: object):
-    loc_keys, duplicated_keys = Localization.get_all_loc_keys(test_runner=test_runner, return_duplicated_keys=True)
+    loc_keys, duplicated_keys = Localization.get_all_loc_keys(test_runner=test_runner, return_duplicated_keys=True, lowercase=False)
 
     ResultsReporter.report_results(results=duplicated_keys, message="Duplicated loc keys were encountered. Check console output")
