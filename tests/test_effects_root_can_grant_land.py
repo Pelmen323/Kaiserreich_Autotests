b45f07c836d702ab1f0d5b3965ba56dcad6d8d57
Pelmen323 <nozovka9@mail.ru>
2023-02-08 15:58:24 +0400

    Rework and restore root_can_grant_land test

--- tests/test_effects_root_can_grant_land.py
+++ tests/test_effects_root_can_grant_land.py
@@ -7,22 +7,22 @@ import os
 import re
 
 from ..test_classes.generic_test_class import FileOpener, ResultsReporter
+from ..test_classes.events_class import Events
 
 
 def test_check_root_can_grant_land_in_state_scope(test_runner: object):
-    filepath = test_runner.full_path_to_mod
+    events = Events.get_all_events(test_runner=test_runner, lowercase=True, return_paths=False)
     results = []
     key_string = "root_can_grant_land"
     pattern = '(^(\\t+)every_owned_state = \\{.*?^\\2\\})'
 
-    for filename in glob.iglob(filepath + '**/*.txt', recursive=True):
-        text_file = FileOpener.open_text_file(filename)
-        if key_string in text_file:
-
-            pattern_matches = re.findall(pattern, text_file, flags=re.DOTALL | re.MULTILINE)
+    for event in events:
+        if key_string in event:
+            event_id = re.findall('^\\tid = ([^ \\n\\t]+)', event, flags=re.MULTILINE)[0]
+            pattern_matches = re.findall(pattern, event, flags=re.DOTALL | re.MULTILINE)
             if len(pattern_matches) > 0:
                 for match in pattern_matches:
                     if key_string in match[0]:
-                        results.append((match[0].replace('\t', '').replace('\n', '  '), os.path.basename(filename)))
+                        results.append(event_id)
 
-    ResultsReporter.report_results(results=results, message="Root can grant land is used in state scope. This will cause errors in log.")
+    ResultsReporter.report_results(results=results, message="Root can grant land is used in state scope. This will cause errors in log and will break the trigger.")
