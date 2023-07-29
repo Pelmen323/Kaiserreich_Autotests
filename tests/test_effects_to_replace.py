e7d2dbb871133775231a6179e9533851616954ea
Pelmen323 <Vadzim_Fiadotsyeu@epam.com>
2023-07-29 14:27:37 +0400

    Add a test to detect if building is spawned before adding slots

--- tests/test_effects_to_replace.py
+++ tests/test_effects_to_replace.py
@@ -14,6 +14,7 @@ input_list = [
     ["set_state_category", ".*set_state_category = [^o].*", "Singleline", "set_state_category is encountered - use 'increase_state_category_by_one_level = yes' instead"],
     ["add_research_slot", ".*add_research_slot = 1.*", "Singleline", "add_research_slot usage is encountered - use 'add_research_slot_until_four/five/six = yes' instead"],
     ["modifier = embargo", ".*modifier = embargo.*", "Singleline", "modifier = embargo usage is encountered - use 'embargo_ROOT/clear_embargo_ROOT/embargo_PREV/clear_embargo_PREV/embargo_FROM/clear_embargo_FROM' instead"],
+    ["building slots", "add_building_construction = \\{[^\\}]+?\\}\\n\\t+add_extra_state_shared_building_slots", "Multiline", "add slots before buildings"]
     # ["any_neighbor_country", ".*any_neighbor_country[^\\}]*\\n.*tag =.*", "Singleline", "use is_neighbor_of instead"],
 ]
 
