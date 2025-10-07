5b090176e28f479086892353bde8d1c9d0c98c6f
Pelmen323 <Vadzim_Fiadotsyeu@epam.com>
2025-10-07 16:22:51 +0300

    fixup! Huge refactor and optimisation of variables tests

--- tests/variables/test_flags_unused.py
+++ tests/variables/test_flags_unused.py
@@ -34,7 +34,7 @@ input_list = [
 
 
 @pytest.mark.parametrize("input_list", input_list)
-def test_unused_country_flags(test_runner: object, input_list):
+def test_unused_flags(test_runner: object, input_list):
     flag_type = input_list[0]
     false_positives = input_list[1]
     results = []
