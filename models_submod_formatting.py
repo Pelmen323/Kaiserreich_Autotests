c1f34208bce3ee2f913293342f0ab5a3bf805b00
Pelmen323 <Vadzim_Fiadotsyeu@epam.com>
2025-12-05 16:31:00 +0300

    Refactor model formatting script

--- models_submod_formatting.py
+++ models_submod_formatting.py
@@ -1,34 +1,34 @@
-import glob
 import re
-from collections import Counter
+from pathlib import Path
+INPUT_FILE = 'C:\\Users\\VADIM\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\Kaiserreich Models Dev Build\\gfx\\entities\\z_units_KR_AMA.asset'
 
-"""
-Game log processing script
-"""
 
-INPUT_FOLDER = 'C:\\Users\\VADIM\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\Kaiserreich Models Dev Build\\gfx\\entities\\z_units_KR_GER.asset'
+def format_models_submod(input_file: str):
+    """Format entity definitions in asset files to have consistent spacing and ordering."""
 
-with open(INPUT_FOLDER, 'r', encoding='utf-8') as file:
-    text_file = file.read()
     replacement_dict = {}
+    file_path = Path(input_file)
+    text_file = file_path.read_text(encoding='utf-8')
     entity_match = re.findall(r'entity = \{[^\}]+\}', text_file, flags=re.DOTALL | re.MULTILINE)
     if len(entity_match) > 0:
         for i in entity_match:
-            if 'attach' in i:
-                continue
-            try:
-                name_match = re.findall(r'name = ".*?"', i)[0]
-                pdxmesh_match = re.findall(r'pdxmesh = ".*?"', i)[0]
-                clone_match = re.findall(r'clone = ".*?"', i)[0]
-            except Exception:
-                continue
+            if 'attach' not in i:
+                try:
+                    name_match = re.findall(r'name = ".*?"', i)[0]
+                    pdxmesh_match = re.findall(r'pdxmesh = ".*?"', i)[0]
+                    clone_match = re.findall(r'clone = ".*?"', i)[0]
+                except IndexError:
+                    continue
 
-            assembled_replacement = f"entity = {{ {name_match:<55}{clone_match:<50}{pdxmesh_match} }}"
-            replacement_dict[i] = assembled_replacement
+                assembled_replacement = f"entity = {{ {name_match:<55}{clone_match:<50}{pdxmesh_match} }}"
+                replacement_dict[i] = assembled_replacement
 
-text_file_new = text_file
-for i in replacement_dict:
-    text_file_new = text_file_new.replace(i, replacement_dict[i])
+    text_file_new = text_file
+    for i in replacement_dict:
+        text_file_new = text_file_new.replace(i, replacement_dict[i])
 
-with open(INPUT_FOLDER, 'w', encoding='utf-8') as text_file_write:
-    text_file_write.write(text_file_new)
+    file_path.write_text(text_file_new, encoding='utf-8')
+
+
+if __name__ == '__main__':
+    format_models_submod(INPUT_FILE)
